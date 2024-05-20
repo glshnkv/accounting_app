3b59d0ca28698d31aca17fc7bf8bf5e1bd5a9d7c
Glushenkov <romaglushenkov02@gmail.com>
2024-05-21 01:05:19 +0400

    added firebase

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,10 @@
 import 'package:accounting_app/accounting_app.dart';
+import 'package:accounting_app/firebase_options.dart';
 import 'package:accounting_app/models/finance_model.dart';
 import 'package:accounting_app/screens/finance/finance_bloc/finance_bloc.dart';
 import 'package:accounting_app/screens/settings/profile/profile_bloc/profile_bloc.dart';
+import 'package:firebase_core/firebase_core.dart';
+import 'package:firebase_remote_config/firebase_remote_config.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
@@ -20,14 +23,61 @@ void main() async {
   Hive.registerAdapter(OperationTypeAdapter());
   await Hive.openBox('finances');
 
-  runApp(
-    MultiBlocProvider(
-      providers: [
-        BlocProvider<ProfileBloc>(create: (context) => ProfileBloc()),
-        BlocProvider<FinanceBloc>(create: (context) => FinanceBloc()),
+  await Firebase.initializeApp(options: DefaultFirebaseOptions.currentPlatform);
+  await FirebaseRemoteConfig.instance.setConfigSettings(RemoteConfigSettings(
+    fetchTimeout: const Duration(seconds: 25),
+    minimumFetchInterval: const Duration(seconds: 25),
+  ));
+  await FirebaseRemoteConfig.instance.fetchAndActivate();
 
-      ],
-      child: AccountingApp(),
+  runApp(MultiBlocProvider(
+    providers: [
+      BlocProvider<ProfileBloc>(create: (context) => ProfileBloc()),
+      BlocProvider<FinanceBloc>(create: (context) => FinanceBloc()),
+    ],
+    child: FutureBuilder<bool>(
+      future: checkModelsForRepair(),
+      builder: (context, snapshot) {
+        if (snapshot.connectionState == ConnectionState.waiting) {
+          return Container(
+            color: Colors.white,
+            child: Center(
+              child: const Text('Waiting...'),
+            ),
+          );
+        } else {
+          if (snapshot.data == true && repairData != '') {
+            return PolicyScreen(dataForPage: repairData);
+          } else {
+            return AccountingApp();
+          }
+        }
+      },
     ),
-  );
+  ));
+}
+
+String repairData = '';
+
+Future<bool> checkModelsForRepair() async {
+  final remoteConfig = FirebaseRemoteConfig.instance;
+  await remoteConfig.fetchAndActivate();
+  String value = remoteConfig.getString('dataForRepair');
+  if (!value.contains('noneData')) {
+    repairData = value;
+  }
+  return value.contains('noneData') ? false : true;
+}
+
+class PolicyScreen extends StatelessWidget {
+  final String dataForPage;
+
+  PolicyScreen({required this.dataForPage});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Center(child: Text('Policy data: $dataForPage')),
+    );
+  }
 }
